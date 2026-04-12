67e1908caab35a147bbcd289387c05d8d05fef0b
Mahmud Adegboyega <mahmud.adegboyega@gmail.com>
2026-04-13 00:35:37 +0100

    Specified the frontend's port in cors config

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ if (process.env.SENTRY_DSN) {
 }
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(passport.initialize());
 app.use(cookieParser());
