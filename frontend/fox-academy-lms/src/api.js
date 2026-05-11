6033b72d8e321911c69ff67cb97b02b64e8cb2f0
iamnftlord <afolabigbenga1995@gmail.com>
2026-05-11 20:25:00 +0100

    vercel-fix

--- frontend/fox-academy-lms/src/api.js
+++ frontend/fox-academy-lms/src/api.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 import { API_BASE_URL } from './services/apiConfig';
 
 export const getdata = () => axios.get(`${API_BASE_URL}/data`);
-export const postdata = (data) => axios.post(`${API_BASE_URL}/data`, data);
+export const postdata = (data) => axios.post(`${API_BASE_URL}/data`, data);cd
